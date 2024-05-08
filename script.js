12138c7a955483f053ba3bfe64a687a10dcf0437
Abhishek Chourasia <110232075+AbhishekChourasia29@users.noreply.github.com>
2024-05-08 15:25:08 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
+// Certificate images data
+const certificateImages = [
+  "img/certificate1.png",
+  "img/certificate2.png",
+  "img/certificate12.png",
+  "img/certificate3.png",
+  "img/certificate13.png",
+  "img/certificate4.png",
+  "img/certificate5.png",
+  "img/certificate6.png",
+  "img/certificate7.png",
+  "img/certificate8.png",
+  "img/certificate9.png",
+  "img/certificate11.png"
+];
+
+// Function to dynamically add certificate images
+function addCertificateImages() {
+  const certificateGallery = document.getElementById('certificate-gallery');
+  certificateImages.forEach(image => {
+      const img = document.createElement('img');
+      img.src = image;
+      img.alt = 'Certificate';
+      certificateGallery.appendChild(img);
+  });
+}
+
+// Call the function to add certificate images when the page loads
+window.onload = addCertificateImages;
+
+// JavaScript to handle video playback
+const projects = document.querySelectorAll('.project');
+
+projects.forEach(project => {
+    const videoWrapper = project.querySelector('.video-wrapper');
+    const videoOverlay = project.querySelector('.video-overlay');
+    const iframe = project.querySelector('iframe');
+
+    videoOverlay.addEventListener('click', () => {
+        const currentVideo = document.querySelector('.playing');
+        if (currentVideo && currentVideo !== videoWrapper) {
+            const currentIframe = currentVideo.querySelector('iframe');
+            currentIframe.removeAttribute('src');
+            currentVideo.classList.remove('playing');
+            currentVideo.querySelector('.video-overlay').style.display = 'block';
+        }
+        
+        if (!videoWrapper.classList.contains('playing')) {
+            iframe.setAttribute('src', `https://www.youtube.com/embed/${project.dataset.videoId}`);
+            videoWrapper.classList.add('playing');
+            videoOverlay.style.display = 'none';
+        }
+    });
+
+    iframe.addEventListener('mouseout', () => {
+        iframe.removeAttribute('src');
+        videoWrapper.classList.remove('playing');
+        videoOverlay.style.display = 'block';
+    });
+});
+
 
